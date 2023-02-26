2c79890dc9e29923708b24cb537991d3224d3fc4
HajarAbuRmeileh <hajar.maint@gmail.com>
2023-02-26 14:26:08 +0200

    first commit22

---
+++ import pytest.py
@@ -0,0 +1,14 @@
+import pytest
+from MyCode import add_numbers
+
+def test_add_positive():
+    assert add_numbers(1,2)==3
+
+def test_add_zero():
+    assert add_numbers(1,0)==1
+
+def test_add_negative():
+        assert add_numbers(5,-100)==-95
+
+
+
